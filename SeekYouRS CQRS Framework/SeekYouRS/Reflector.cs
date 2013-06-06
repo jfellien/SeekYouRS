837bb3bbe0fa60b4b16188256f1aeed3ce779ddc
Carsten König <Carsten@gettingsharper.de>
2013-06-06 07:10:18 +0200

    Reflector cached jetzt die Eigenschaftsleser statt jedesmal Reflection zu benutzen

--- SeekYouRS CQRS Framework/SeekYouRS/Reflector.cs
+++ SeekYouRS CQRS Framework/SeekYouRS/Reflector.cs
@@ -1,15 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+
+using ValueGetterKey = System.Tuple<System.Type, string, System.Type>;
+using ValueGetter = System.Func<object, object>;
 
 namespace SeekYouRS
 {
     public static class Reflector
     {
-        static bool TryReadProperty<tObject, tResult>(tObject obj, string propertyName, out tResult result)
+        public static void ClearCache()
         {
-            result = default(tResult);
-            var propertyInfos = obj.GetType().GetProperties();
+            _valueGetters.Clear();
+        }
 
+        public static tResult ReadValueOrDefault<tObject, tResult>(tObject obj, string name, tResult defaultValue = default(tResult))
+        {
+            var type = obj.GetType();
+            var resultType = typeof(tResult);
+            var key = Tuple.Create(type, name, resultType);
+            ValueGetter getter;
+
+            if (_valueGetters.TryGetValue(key, out getter))
+                return (tResult)getter(obj);
+
+            if (!TryGetPropertyReader(type, resultType, name, out getter)
+                && !TryGetFieldReader(type, resultType, name, out getter))
+                getter = _ => defaultValue;
+
+            _valueGetters.Add(key, getter);
+            return (tResult)getter(obj);
+        }
+
+        private static readonly Dictionary<ValueGetterKey, ValueGetter> 
+            _valueGetters = new Dictionary<ValueGetterKey, ValueGetter>();
+
+        static bool TryGetPropertyReader(Type type, Type resultType, string propertyName, out Func<object, object> propertyReader)
+        {
+            propertyReader = _ => { throw new NotSupportedException(); };
+
+            var propertyInfos = type.GetProperties();
             try
             {
                 var identifier =
@@ -18,8 +48,10 @@ namespace SeekYouRS
                                              StringComparison.OrdinalIgnoreCase));
 
                 if (identifier == null) return false;
+                if (!resultType.IsAssignableFrom(identifier.PropertyType)) return false;
 
-                result = (tResult) identifier.GetValue(obj);
+                propertyReader = identifier.GetValue;
+
                 return true;
             }
             catch (Exception)
@@ -28,38 +60,28 @@ namespace SeekYouRS
             }
         }
 
-        static bool TryReadField<tObject, tResult>(tObject obj, string fieldName, out tResult result)
+        static bool TryGetFieldReader(Type type, Type resultType, string fieldName, out Func<object, object> fieldReader)
         {
-            result = default(tResult);
-            var filedInfos = obj.GetType().GetFields();
+            fieldReader = _ => { throw new NotSupportedException(); };
 
+            var fieldInfos = type.GetFields();
             try
             {
                 var identifier =
-                    filedInfos.SingleOrDefault(
+                    fieldInfos.SingleOrDefault(
                         pi => pi.Name.Equals(fieldName,
                                              StringComparison.OrdinalIgnoreCase));
 
                 if (identifier == null) return false;
+                if (!resultType.IsAssignableFrom(identifier.FieldType)) return false;
+                fieldReader = identifier.GetValue;
 
-                result = (tResult)identifier.GetValue(obj);
                 return true;
             }
             catch (Exception)
             {
                 return false;
             }
-        }
-
-        public static tResult ReadValueOrDefault<tObject, tResult>(tObject obj, string name, tResult defaultValue = default(tResult))
-        {
-            tResult result;
-
-            if (TryReadField(obj, name, out result)
-                || TryReadProperty(obj, name, out result))
-                return result;
-
-            return defaultValue;
         }
     }
 }
